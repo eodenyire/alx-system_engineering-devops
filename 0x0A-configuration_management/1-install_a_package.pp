f6ffc2a1c9c07d7c7d351bdb2dfa9244748fb46e
Emmanuel Odenyire Anyira <61192633+eodenyire@users.noreply.github.com>
2024-06-28 20:33:24 +0300

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# This Puppet manifest installs Flask version 2.1.0 using pip3.
-package { 'Flask':
+# Installs flask, version 2.1.0.
+
+package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
