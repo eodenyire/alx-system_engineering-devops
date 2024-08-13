d76e3303d06cb51cea8b13576f2424f3f3cfa332
Emmanuel Odenyire Anyira <61192633+eodenyire@users.noreply.github.com>
2024-08-13 20:44:57 +0300

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 """
+Function that queries the Reddit API and returns the number of subscribers
+(not active users, total subscribers) for a given subreddit.
+If an invalid subreddit is given, the function should return 0
+
 0-subs.py
 
 This module contains the function number_of_subscribers 
