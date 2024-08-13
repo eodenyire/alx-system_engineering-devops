e442b7d675537a583dc5d61e8871b0c1518abb28
Emmanuel Odenyire Anyira <61192633+eodenyire@users.noreply.github.com>
2024-08-13 20:57:04 +0300

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,27 +1,22 @@
 #!/usr/bin/python3
 """
-Function that queries the Reddit API and prints the titles
-of the first 10 hot posts listed for a given subreddit.
+Module 1-top_ten that contains the function top_ten
 """
-
 import requests
 
 
 def top_ten(subreddit):
     """
-    Function that queries the Reddit API
-    - If not a valid subreddit, print None.
+    function that queries the Reddit API and prints the
+    titles of the first 10 hot posts listed for a given
+    subreddit
     """
-    req = requests.get(
-        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
-        headers={"User-Agent": "Custom"},
-        params={"limit": 10},
-    )
-
-    if req.status_code == 200:
-        for get_data in req.json().get("data").get("children"):
-            dat = get_data.get("data")
-            title = dat.get("title")
-            print(title)
-    else:
-        print(None)
+    url = 'http://www.reddit.com/r/{}/hot/.json'.format(subreddit)
+    headers = {'User-Agent': '0x16-api_advanced:project:v1.0.0'}
+    params = {'limit': 10}
+    r = requests.get(url, headers=headers, params=params)
+    if r.status_code == 200:
+        data = r.json().get('data')
+        [print(child["data"]["title"]) for child in data["children"]]
+        return
+    print('None')
